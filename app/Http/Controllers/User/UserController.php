6116db8f2124d08a6f1ed7cbb395e0491f396ddd
Your Name <lianshijie@3023668879.com>
2019-05-14 10:25:49 +0800

    user

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+class UserController extends Controller
+{
+    
+}
+?>
