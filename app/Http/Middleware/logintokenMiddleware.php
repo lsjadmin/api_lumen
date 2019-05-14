6be811bb04df89cd968e64f644a89d1850eccc86
Your Name <lianshijie@3023668879.com>
2019-05-14 20:00:46 +0800

    debug

--- app/Http/Middleware/logintokenMiddleware.php
+++ app/Http/Middleware/logintokenMiddleware.php
@@ -16,15 +16,29 @@ class logintokenMiddleware
     public function handle($request, Closure $next)
     {
         $token=$_GET['token'];
-        echo $token;
+        //echo $token;
         $api_id=$_GET['api_id'];
-        echo $api_id;
+        //echo $api_id;
         if(empty($token)||empty($api_id)){
             die('参数不同');
         }
         $key="lumen_login_token.$api_id";
         $redis_token=Redis::get($key);
-        echo $redis_token;
+        //echo $redis_token;
+        if($token==$redis_token){
+            $arr=[
+                'res'=>40001,
+                'msg'=>'token 一致 登陆成功'
+            ];
+            return json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }else{
+            $arr=[
+                'res'=>40002,
+                'msg'=>'token 不一致 登陆失败'
+
+            ];
+            return json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }
         return $next($request);
     }
 }
