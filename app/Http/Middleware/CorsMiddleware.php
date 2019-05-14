60e2ca17bd597d47c4ea9f3e08c0a317644c4155
Your Name <lianshijie@3023668879.com>
2019-05-14 19:08:06 +0800

    debug

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,11 +15,26 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if($request->isMethod('OPTIONS')){
-            $response=response('');
-        }else{
-            $response=$next($request);
+
+        if($request->getMethod() == "OPTIONS") {
+            $allowOrigin = [
+                'http://192.168.40.129',
+                'http://localhost',
+            ];
+            $Origin = $request->header("Origin");
+            if(in_array($Origin, $allowOrigin)){
+                return response()->json('ok', 200, [
+                    # 下面参数视request中header而定
+                    'Access-Control-Allow-Origin' => $Origin,
+                    'Access-Control-Allow-Headers' => 'x-token',
+                    'Access-Control-Allow-Methods' => 'GET,POST,OPTIONS']);
+            } else {
+                return response()->json('fail', 405);
+            }
         }
+
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
         return $response;
     }
 }
